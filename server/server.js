46903cbebb7600eedbf3cd1461fe905bbe728b6f
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-08-14 16:12:07 +0530

    typo error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
-import employeeRouter from "./routes/Employee.js";
+import employeeRouter from "./routes/employee.js";
 import adminRouter from "./routes/admin.js";
 
 

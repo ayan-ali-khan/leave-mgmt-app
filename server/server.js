cadef3f3e32986cccf431e8c506e897b758905e3
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-08-14 16:04:04 +0530

    error resolving

--- server/server.js
+++ server/server.js
@@ -17,7 +17,15 @@ app.use(express.json());
 app.use(cors());
 
 //connect to DB
-await connectDB();
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "Database connection failed" });
+  }
+});
+
 
 app.get('/', (req, res) => res.send("Server is live"))
 
@@ -28,10 +36,10 @@ app.use("/api/employee", employeeRouter);
 app.use("/api/admin", adminRouter);
 
 
-if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, ()=> console.log("Server is running on port: " + PORT));
-}
+// if(process.env.NODE_ENV !== "production"){
+//     const PORT = process.env.PORT || 5000;
+//     app.listen(PORT, ()=> console.log("Server is running on port: " + PORT));
+// }
 
 //export server for vercel
 export default app;
